c48f8c27bf45ce2c5c16e2c6a9e743ffe3e8eae5
kataoka masahiro <m.kataoka.ro@gmail.com>
2020-03-02 20:01:10 +0900

    [add] fx

--- fx/instruments_candles.py
+++ fx/instruments_candles.py
@@ -9,6 +9,8 @@ import datetime
 import json
 import pandas as pd
 from sqlalchemy import create_engine
+import time
+from tqdm import tqdm
 
 account_id = oak.ACCOUNT_ID
 access_token = oak.PERSONAL_ACCESS_TOKEN
@@ -16,40 +18,42 @@ access_token = oak.PERSONAL_ACCESS_TOKEN
 api = oandapyV20.API(access_token = access_token, environment = "practice")
 
 datetime_format = "%Y-%m-%dT%H:%M:%S.%f%z"
-base_datetime = datetime.datetime(year=2019, month=11, day=1)
+start_datetime = datetime.datetime(year=2019, month=1, day=1)
+end_datetime = datetime.datetime(year=2020, month=2, day=29)
+date_window = 7
 
-ic = InstrumentsCandles(
-		instrument="USD_JPY",
-		params={
-			"granularity": consts.Granularity.M5,
-			"alignmentTimezone": "Japan",
-			"count": 5000
-			# "from": base_datetime.strftime(datetime_format),
-			# "to": (base_datetime + relativedelta(months=1) - relativedelta(seconds=1)).strftime(datetime_format)
-		}
-	)
+while (start_datetime + relativedelta(days=date_window)) <= end_datetime:
+	ic = InstrumentsCandles(
+			instrument="USD_JPY",
+			params={
+				"granularity": consts.Granularity.M15,
+				"alignmentTimezone": "Japan",
+				# "count": 5000
+				"from": start_datetime.strftime(datetime_format),
+				"to": (start_datetime + relativedelta(days=date_window) - relativedelta(seconds=1)).strftime(datetime_format)
+			}
+		)
+	start_datetime = start_datetime + relativedelta(days=date_window)
+	api.request(ic)
+	data = []
+	for candle in ic.response["candles"]:
+		data.append(
+			[
+				candle['time'],
+				candle['volume'],
+				candle['mid']['o'],
+				candle['mid']['h'],
+				candle['mid']['l'],
+				candle['mid']['c']
+			]
+		)
 
-api.request(ic)
-
-data = []
-for candle in ic.response["candles"]:
-	data.append(
-		[
-			candle['time'],
-			candle['volume'],
-			candle['mid']['o'],
-			candle['mid']['h'],
-			candle['mid']['l'],
-			candle['mid']['c']
-		]
-	)
-
-df = pd.DataFrame(data)
-df.columns = ['datetime', 'volume', 'open', 'high', 'low', 'close']
-df = df.set_index('datetime')
-df.index = pd.to_datetime(df.index)
-# print(df.head())
-# print(df.isna().all())
-
-engine = db_access.get_engine()
-df.to_sql('usd_jpy', con=engine, if_exists='append')
+	df = pd.DataFrame(data)
+	df.columns = ['datetime', 'volume', 'open', 'high', 'low', 'close']
+	df = df.set_index('datetime')
+	df.index = pd.to_datetime(df.index)
+	# print(df.head())
+	# print(df.isna().all())
+	engine = db_access.get_engine()
+	df.to_sql('usd_jpy_15m', con=engine, if_exists='append')
+	time.sleep(1)
